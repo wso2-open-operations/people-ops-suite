670dc261f5355368a905d2197a41f806877118df
Dineth De Silva <dinethran33@gmail.com>
2025-09-20 11:45:54 +0530

    feat: add link to profile and adjust mecard based on sidebar state

--- apps/people-app/webapp-template/src/component/ui/MeCard.tsx
+++ apps/people-app/webapp-template/src/component/ui/MeCard.tsx
@@ -1,8 +1,12 @@
 import { RootState, useAppSelector } from "@root/src/slices/store";
+import { Link } from "react-router-dom";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@component/common/avatar";
 
-function MeCard() {
+interface MeCardProps {
+  sidebarOpen: boolean;
+}
+function MeCard({ sidebarOpen }: MeCardProps) {
   const user = useAppSelector((state: RootState) => state.user);
 
   const userName: string | undefined =
@@ -19,21 +23,34 @@ function MeCard() {
       : "name";
 
   return (
-    <div className="bg-secondary-default p-6 ">
-      <div className="flex gap-3 bg-[hsla(0,0%,97%,0.08)] outline-1 outline-offset-[-1px] outline-neutral-200/10 p-2 rounded-lg">
-        <Avatar className="p-5 outline-1 outline-offset-[-1px] outline-neutral-100/10 ">
-          <AvatarImage className="h-8 w-8 " src={`${user.userInfo?.employeeThumbnail}`} />
-          <AvatarFallback>
-            <p className="p-r text-st-nav-clicked ">{initials}</p>
-          </AvatarFallback>
-        </Avatar>
+    <Link to={"/profile"}>
+      <div>
+        {sidebarOpen ? (
+          <div className="flex gap-3 bg-[hsla(0,0%,97%,0.08)] outline-1 outline-offset-[-1px] outline-neutral-200/10 p-2 rounded-lg">
+            <Avatar className="p-5 outline-[1.6px] outline-offset-[-1px] outline-neutral-100/10 ">
+              <AvatarImage className="h-8 w-8 " src={`${user.userInfo?.employeeThumbnail}`} />
+              <AvatarFallback>
+                <p className="p-r text-st-nav-clicked ">{initials}</p>
+              </AvatarFallback>
+            </Avatar>
 
-        <div className="">
-          <p className="p-m text-st-nav-clicked">{userName}</p>
-          <p className="p-s text-st-nav-link">{user.userInfo?.workEmail || ""}</p>
-        </div>
+            <div className="">
+              <p className="p-m text-st-nav-clicked">{userName}</p>
+              <p className="p-s text-st-nav-link">{user.userInfo?.workEmail || ""}</p>
+            </div>
+          </div>
+        ) : (
+          <div className="py-2">
+            <Avatar className="p-5 outline-[1.6px] outline-offset-[-1px] outline-st-nav-link ">
+              <AvatarImage className="h-8 w-8 " src={`${user.userInfo?.employeeThumbnail}`} />
+              <AvatarFallback>
+                <p className="p-r text-st-nav-clicked ">{initials}</p>
+              </AvatarFallback>
+            </Avatar>
+          </div>
+        )}
       </div>
-    </div>
+    </Link>
   );
 }
 
