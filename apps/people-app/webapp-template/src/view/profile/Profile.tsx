1bcf39fabf4eed023febcbc03eb24eda6b0968e9
Dineth De Silva <dinethran33@gmail.com>
2025-09-20 12:15:40 +0530

    feat: implement profile page with tabbed layout

--- apps/people-app/webapp-template/src/view/profile/Profile.tsx
+++ apps/people-app/webapp-template/src/view/profile/Profile.tsx
@@ -1,8 +1,44 @@
+import TabsPage from "@root/src/layout/pages/TabsPage";
+// import { fetchEmployeeInfo } from "@root/src/slices/employeeSlice/employee";
+import { useAppDispatch } from "@root/src/slices/store";
+import { Files, Landmark, SquareUserRound, UserLock } from "lucide-react";
+
+import EmployeeInfo from "./panel/EmployeeInfo";
+
 function Profile() {
+  const dispatch = useAppDispatch();
+
+  // dispatch(fetchEmployeeInfo());
   return (
-    <div>
-      <div></div>
-    </div>
+    <TabsPage
+      title="Profile"
+      tabsPage={[
+        {
+          tabTitle: "Employee Info",
+          tabPath: "employee-info",
+          icon: <SquareUserRound />,
+          page: <EmployeeInfo />,
+        },
+        {
+          tabTitle: "Personal Info",
+          tabPath: "personal-info",
+          icon: <UserLock />,
+          page: <EmployeeInfo />,
+        },
+        {
+          tabTitle: "Documents",
+          tabPath: "documents",
+          icon: <Files />,
+          page: <EmployeeInfo />,
+        },
+        {
+          tabTitle: "Bank Details",
+          tabPath: "bank-details",
+          icon: <Landmark />,
+          page: <EmployeeInfo />,
+        },
+      ]}
+    />
   );
 }
 
