677b3f43754fdc1aa8e5d0d5a29df79d57e8a47b
Saajid Ahamed <saajidahamed1@gmail.com>
2025-12-01 01:02:23 +0530

    feat: implement date pill component to contain "Full/First Half/Second Half" date values

---
+++ apps/leave-app/webapp/src/view/GeneralLeave/DatePill.tsx
@@ -0,0 +1,45 @@
+// Copyright (c) 2025 WSO2 LLC. (https://www.wso2.com).
+//
+// WSO2 LLC. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+import { Box, Typography } from "@mui/material";
+
+interface DatePillProps {
+  partOfDay: string;
+}
+
+export default function DatePill({ partOfDay }: DatePillProps) {
+  return (
+    <Box
+      py={1}
+      width={1}
+      borderRadius="0.5rem"
+      textAlign="center"
+      border="solid"
+      borderColor="lightgrey"
+      maxWidth="20rem"
+      sx={{
+        "&:active": {
+          backgroundColor: "#ff7300",
+          color: "white",
+        },
+        "&:hover": {
+          cursor: "pointer",
+        },
+      }}
+    >
+      <Typography variant="subtitle2">{partOfDay}</Typography>
+    </Box>
+  );
+}
