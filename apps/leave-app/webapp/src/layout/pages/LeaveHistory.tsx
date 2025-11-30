db1cdbb6c417d1c9a1f5a3d223ed1519598b9fab
Saajid Ahamed <saajidahamed1@gmail.com>
2025-12-01 01:00:05 +0530

    feat: add leave history page

--- apps/leave-app/webapp/src/layout/pages/LeaveHistory.tsx
+++ apps/leave-app/webapp/src/layout/pages/LeaveHistory.tsx
@@ -13,13 +13,8 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
-import { Typography } from "@mui/material";
+import Title from "@root/src/component/common/Title";
 
 export default function LeaveHistory() {
-  return (
-      <Typography variant="h4" style={{ color: "gray" }}>
-        Leave History Page
-      </Typography>
-  );
+  return <Title firstWord="Leave" secondWord="History" />;
 }
