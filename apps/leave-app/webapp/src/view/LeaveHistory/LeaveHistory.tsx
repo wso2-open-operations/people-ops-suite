f4fc0d5a3dfcd25b4476d428a9c413218dfa04dc
Saajid Ahamed <saajidahamed1@gmail.com>
2025-12-03 17:26:06 +0530

    feat: implement leave history page

--- apps/leave-app/webapp/src/view/LeaveHistory/LeaveHistory.tsx
+++ apps/leave-app/webapp/src/view/LeaveHistory/LeaveHistory.tsx
@@ -14,8 +14,33 @@
 // specific language governing permissions and limitations
 // under the License.
 
+import { Box, Stack } from "@mui/material";
+
 import Title from "@root/src/component/common/Title";
+import { PAGE_MAX_WIDTH } from "@root/src/config/ui";
+
+import { mockLeaveHistory } from "./MockData";
+import LeaveCard from "./component/LeaveCard";
 
 export default function LeaveHistory() {
-  return <Title firstWord="Leave" secondWord="History" />;
+  return (
+    <Stack maxWidth={PAGE_MAX_WIDTH} margin="auto" gap="1.5rem">
+      <Title firstWord="Leave" secondWord="History" />
+      <Box gap="2rem" display="grid" gridTemplateColumns={{ xs: "1fr", md: "1fr 1fr" }}>
+        {mockLeaveHistory.map((leave) => (
+          <LeaveCard
+            key={leave.id}
+            id={leave.id}
+            type={leave.type}
+            startDate={leave.startDate}
+            endDate={leave.endDate}
+            duration={leave.duration}
+            status={leave.status}
+            month={leave.month}
+            day={leave.day}
+          />
+        ))}
+      </Box>
+    </Stack>
+  );
 }
