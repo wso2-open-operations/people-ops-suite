f2fcf238ffad41de307060eb8f359d7d82733dc4
dinethde <dinethran33@gmail.com>
2025-12-08 10:25:48 +0530

    feat: refactor menu API service to use baseQueryWithRetry and streamline code

---
+++ apps/menu-app/webapp/src/services/menu.api.ts
@@ -0,0 +1,23 @@
+// RTK Query API for menu-related endpoints
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+import type { Menu } from "@/types/types";
+
+import { baseQueryWithRetry } from "./BaseQuery";
+
+export const menuApi = createApi({
+  reducerPath: "menuApi",
+  baseQuery: baseQueryWithRetry,
+  tagTypes: ["Menu"],
+  endpoints: (builder) => ({
+    /**
+     * Fetch application menu structure
+     */
+    getMenu: builder.query<Menu, void>({
+      query: () => "menu",
+      providesTags: ["Menu"],
+    }),
+  }),
+});
+
+export const { useGetMenuQuery } = menuApi;
