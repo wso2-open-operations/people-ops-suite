f9e816cba28289f42f5d903ebabecdc05d7e6024
dinethde <dinethran33@gmail.com>
2026-01-07 00:02:32 +0530

    feat: adds Apache license headers to API service files

--- apps/menu-app/webapp/src/services/dod.api.ts
+++ apps/menu-app/webapp/src/services/dod.api.ts
@@ -12,6 +12,7 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
+// under the License.
 import { createApi } from "@reduxjs/toolkit/query/react";
 
 import { DinnerRequest } from "@/types/types";
