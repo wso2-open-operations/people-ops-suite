a232a56662aa2701ed9ebfbefbace5a9e6f45139
dinethde <dinethran33@gmail.com>
2026-01-12 14:05:20 +0530

    refactor: remove Help & Support route and associated import

--- apps/menu-app/webapp/src/view/index.tsx
+++ apps/menu-app/webapp/src/view/index.tsx
@@ -16,9 +16,7 @@
 import { lazy } from "react";
 
 const home = lazy(() => import("@view/home/home"));
-const help = lazy(() => import("@view/help/help"));
 
 export const View = {
-  help,
   home,
 };
