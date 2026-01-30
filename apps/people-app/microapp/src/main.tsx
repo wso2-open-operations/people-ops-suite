7068ac6a1b11a0bcbea5d5dcea66071678de7131
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2026-01-30 09:39:58 +0530

    chore: update copyright headers from 2025 to 2026

--- apps/people-app/microapp/src/main.tsx
+++ apps/people-app/microapp/src/main.tsx
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 WSO2 LLC. (https://www.wso2.com).
+// Copyright (c) 2026 WSO2 LLC. (https://www.wso2.com).
 //
 // WSO2 LLC. licenses this file to you under the Apache License,
 // Version 2.0 (the "License"); you may not use this file except
@@ -43,5 +43,5 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </StrictMode>
+  </StrictMode>,
 );
