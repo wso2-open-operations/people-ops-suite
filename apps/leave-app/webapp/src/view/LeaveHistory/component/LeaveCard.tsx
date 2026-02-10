8449ca43c0695e28fdfb5971a6f8aedbb52566db
Saajid Ahamed <saajidahamed1@gmail.com>
2026-02-10 20:52:36 +0530

    style: remove fill from calendar icon

--- apps/leave-app/webapp/src/view/LeaveHistory/component/LeaveCard.tsx
+++ apps/leave-app/webapp/src/view/LeaveHistory/component/LeaveCard.tsx
@@ -160,8 +160,8 @@ export default function LeaveCard({
                 {/* Month header */}
                 <Box
                   sx={{
-                    backgroundColor: theme.palette.primary.main,
-                    color: theme.palette.primary.contrastText,
+                    borderBottom: `2px solid ${theme.palette.primary.main}`,
+                    color: theme.palette.text.primary,
                     textAlign: "center",
                     py: "2px",
                     flex: "0 0 auto",
