f87cbdb891f62d91b0d0c570716296b7ee69daee
dinethde <dinethran33@gmail.com>
2026-02-23 00:27:48 +0530

    refactor(BackdropProgress): streamline props and enhance styling flexibility

--- apps/people-app/webapp/src/component/ui/BackdropProgress.tsx
+++ apps/people-app/webapp/src/component/ui/BackdropProgress.tsx
@@ -13,19 +13,16 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+import { Backdrop, BackdropProps, CircularProgress, useTheme } from "@mui/material";
 
-import { Backdrop, CircularProgress, useTheme } from "@mui/material";
+type BackdropProgressProps = BackdropProps;
 
-type BackdropProgressProps = {
-  open: boolean;
-};
-
-const BackdropProgress = ({ open }: BackdropProgressProps) => {
+const BackdropProgress = ({ sx, ...rest }: BackdropProgressProps) => {
   const theme = useTheme();
   return (
     <Backdrop
-      sx={{ zIndex: 500 + 1, color: theme.palette.secondary.contrastText }}
-      open={open}
+      sx={{ zIndex: 500 + 1, color: theme.palette.secondary.contrastText, ...sx }}
+      {...rest}
     >
       <CircularProgress />
     </Backdrop>
