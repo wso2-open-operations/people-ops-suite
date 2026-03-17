c15f5fc74d7e085436c6dbf049dcd2ab07d1cbaf
dinethde <dinethran33@gmail.com>
2026-03-17 12:37:21 +0530

    feat(SplitView): add navigation for search results and update selection logic

--- apps/people-app/webapp/src/view/master-data/panel/split-view-t/hooks/useFindParent.ts
+++ apps/people-app/webapp/src/view/master-data/panel/split-view-t/hooks/useFindParent.ts
@@ -13,7 +13,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-import { NodeType } from "@root/src/utils/types";
+import { NodeType } from "@utils/types";
 import type {
   BusinessUnitState,
   CompanyState,
