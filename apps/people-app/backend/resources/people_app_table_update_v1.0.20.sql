26843b36918383806d0c4c48e74e70c3f0f14318
Ayesh Almeida <ayesh9303@gmail.com>
2026-07-17 09:46:10 +0530

    Add DB script required for ex-employee migration

---
+++ apps/people-app/backend/resources/people_app_table_update_v1.0.20.sql
@@ -0,0 +1,14 @@
+INSERT INTO people_ops_suite.business_unit(name, head_email, is_active, created_by, updated_by) VALUES
+    ('Not Specified', '', 0, 'ayeshalm@wso2.com', 'ayeshalm@wso2.com'), 
+    ('Moseif', '', 0, 'ayeshalm@wso2.com', 'ayeshalm@wso2.com');
+
+INSERT INTO people_ops_suite.companies_allowed_locations(company_id, allowed_location, is_active, created_by, updated_by) VALUES 
+    (1, 'Canada', 0, 'ayeshalm@wso2.com', 'ayeshalm@wso2.com'),
+    (1, 'Columbia', 0, 'ayeshalm@wso2.com', 'ayeshalm@wso2.com'),
+    (1, 'Portugal', 0, 'ayeshalm@wso2.com', 'ayeshalm@wso2.com'),
+    (1, 'US - VP', 0, 'ayeshalm@wso2.com', 'ayeshalm@wso2.com'),
+    (1, 'Sri Lanka', 0, 'ayeshalm@wso2.com', 'ayeshalm@wso2.com'),
+    (1, 'Ukraine', 0, 'ayeshalm@wso2.com', 'ayeshalm@wso2.com'),
+    (2, 'UK - TL', 0, 'ayeshalm@wso2.com', 'ayeshalm@wso2.com'),
+    (6, 'Sri Lanka - TL', 0, 'ayeshalm@wso2.com', 'ayeshalm@wso2.com'),
+    (13, 'Spain - VP', 0, 'ayeshalm@wso2.com', 'ayeshalm@wso2.com');
